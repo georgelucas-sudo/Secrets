528c5f5c8456f0564bc56f0511c87ea928bd59d1
Yiga George Lucas <georgedalucas111@gmail.com>
2023-04-20 18:41:39 +0300

    hashing our users password

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 
@@ -22,7 +22,7 @@ const userschema = new mongoose.Schema({
     password: String
 });
 
-userschema.plugin(encrypt, { secret: process.env.API_KEY, encryptedFields: ["password"] })
+
 
 const User = new mongoose.model("User", userschema);
 
@@ -50,7 +50,7 @@ app.get("/submit", function(req, res) {
 app.post("/register", async(req, res) => {
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
 
     await newUser.save();
@@ -86,7 +86,7 @@ app.post("/register", async(req, res) => {
 });
 app.post("/login", async function(req, res) {
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
     try {
         const foundUser = await User.findOne({ email: username });
         if (foundUser && foundUser.password === password) {
